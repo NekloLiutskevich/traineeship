e96a3dbe0be9387804f82ec8ef3452890ed7eb60
K. Liutskevich <k.liutskevich@gmail.com>
2025-06-27 02:08:39 +0200

    fixed dependencies

---
+++ src/app/app.tsx
@@ -0,0 +1,7 @@
+import React, {FC, type MouseEvent, useEffect, useMemo} from "react";
+
+export const App: FC = () => {
+    return (
+        <h1>Hello, Reacddddddt!</h1>
+    )
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client';
+import {App} from './app'
 import './styles/main.scss'
 
-const App: React.FC = () => <h1>Hello, React!</h1>
-
 const container = document.getElementById('root')
+
 if (container) {
-  const root = createRoot(container)
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
+    const root = createRoot(container)
+
+    root.render(
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    )
 }
